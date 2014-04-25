2a4446fe9d4ba70f97d20dd2dd950850adaf0f36
Javier Miras <jmirasb@gmail.com>
2014-04-25 16:52:52 +0200

    Updating version

--- mkpkg.sh
+++ mkpkg.sh
@@ -5,7 +5,7 @@ rm pkg/*.tar.gz 2>/dev/null
 PLUGIN="odfilemanager"
 VERSION=`cat var/opendomo/plugins/odfilemanager.version`
 USR="--owner 1000 --group 1000 --same-permissions"
-EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE'
+EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE --exclude odfilemanager.png'
 
 rm -fr pkg/*.tar.gz
 tar zcf  pkg/$PLUGIN-$VERSION.tar.gz usr var  $USR $EXCLUDE
