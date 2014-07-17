c80760f256ce8a37ea2e05c1da0ba468925ee0f4
opalenzuela <opalenzuela@gmail.com>
2014-07-17 11:56:38 +0200

    Scan also images!

---
+++ usr/local/opendomo/eventhandlers/scanImageDrive.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#desc: Scan a image drive, like a digital camera 
+
+event="$1"
+module="$2"
+description="$3"
+drive="$4"
+
+
+if test -z "$drive"
+then
+	echo "usage:`basename $0` event module description /path/to/drive"
+	exit 1
+fi
+
+if test -f $drive/.scanfile.pid
+then
+	return 0
+fi
+if touch $drive/.scanfile.pid
+then
+	mkdir -p $drive/.thumbnails/
+	# Indexing files in background
+	for file in `find -type f `; do bn=`basename $file`; convert -quiet -thumbnail 200 $file $bn; done rm $drive/.scanfile.pid & 
+else
+	echo "#ERROR Invalid privileges or readonly drive"
+	return 1
+fi
