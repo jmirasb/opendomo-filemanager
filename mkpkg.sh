67a221c134284e46d94a7043f8a6aa6dc78226f6
Javier Miras Blanco <jmirasb@gmail.com>
2014-09-01 14:52:21 +0200

    Don't use .version file anymore

--- mkpkg.sh
+++ mkpkg.sh
@@ -3,7 +3,7 @@
 mkdir -p pkg
 rm pkg/*.tar.gz 2>/dev/null
 PLUGIN="odfilemanager"
-VERSION=`cat var/opendomo/plugins/odfilemanager.version`
+VERSION=`date '+%Y%m%d'`
 USR="--owner 1000 --group 1000 --same-permissions"
 EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE --exclude odfilemanager.png'
 
