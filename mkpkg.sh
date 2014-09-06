f53d2e8bb3d96efd60da182972077cb2554b4c8c
Javier Miras Blanco <jmirasb@gmail.com>
2014-09-06 11:16:54 +0200

    Updating mkpkg.sh

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-
-mkdir -p pkg
-rm pkg/*.tar.gz 2>/dev/null
-PLUGIN="odfilemanager"
-VERSION=`date '+%Y%m%d'`
-USR="--owner 1000 --group 1000 --same-permissions"
-EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE --exclude odfilemanager.png'
-
-rm -fr pkg/*.tar.gz
-tar zcf  pkg/$PLUGIN-$VERSION.tar.gz usr var  $USR $EXCLUDE
+PKGID="odfilemanager"
+tar cvfz $PKGID-`date '+%Y%m%d'`.tar.gz usr var --owner 1000 --group 1000 --exclude "*~" --exclude .svn --exclude "*.a"
