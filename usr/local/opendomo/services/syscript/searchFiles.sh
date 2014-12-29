17c5965f0b8c9c8afd4b025139982ed2e7ce8781
opalenzuela <opalenzuela@gmail.com>
2014-12-29 13:28:30 +0100

    Testing with new path

--- usr/local/opendomo/services/syscript/searchFiles.sh
+++ usr/local/opendomo/services/syscript/searchFiles.sh
@@ -8,22 +8,38 @@
 SEARCHPATH="$1"
 STRING="$2"
 
-if test -z "$1"
+if test -z "$1" || test $1 = "/"
 then
-	# No parameter specified"
+	# No parameter specified: just show the search form
 	SEARCHPATH="/"
 
 else
-	cd /media/$SEARCHPATH
+	# Case 1: searching in the entire library
+	if test "$SEARCHPATH" = "/"
+	then
+		cd /media/
+		GREPQUERY="/media/*/.scanfile.txt"
+	fi
+
+	# Case 2: searchpath is a drive name
+	if test -d /media/$SEARCHPATH
+	then
+		cd /media/$SEARCHPATH
+		GREPQUERY="/media/$SEARCHPATH/.scanfile.txt"
+	fi
+	
+	# Case 3: searching in a collection
+	if test -f /home/$USER/collections/$SEARCHPATH.col 
+	then
+		source /home/$USER/collections/$SEARCHPATH.col 
+		cd $LOCATION
+		GREPQUERY=".scanfile.txt"
+	fi
+	
 	echo "#> Search results"
 	echo "list:`basename $0`"
-	for d in *
-	do
-		if test "$d" != "*" && test -f $d/.scanfile.txt 
-		then
-			grep $STRING $d/.scanfile.txt | awk '{print "\t-" $d "/" $3 "\t" $3 "\t" "file" "\t" $2 }'
-		fi
-	done
+	grep $STRING $GREPQUERY | awk '{print "\t-" $d "/" $3 "\t" $3 "\t" "file" "\t" $2 }'
+
 fi
 echo
 echo "#> Search"
