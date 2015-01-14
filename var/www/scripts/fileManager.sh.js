8c35c0193bfcad62b56f55a9fe646889783aa977
opalenzuela <opalenzuela@gmail.com>
2015-01-14 14:32:23 +0100

    Error endurance

--- var/www/scripts/fileManager.sh.js
+++ var/www/scripts/fileManager.sh.js
@@ -2,14 +2,28 @@ var currentItem = -1;
 $(function($){
 	$("body").on("keydown",function(event){
 		console.log("Key pressed: "+ event.which);
-		if (event.which == 39 ) { // Right
+		/* NAVIGATION KEYS */
+		if (event.which == 39 ) { // Right 
 			currentItem++;
 		}
 		if (event.which == 37 ) { // Left
 			currentItem--;
 		}	
 		if (currentItem == -1) currentItem = $("fieldset li").length -1;
-		if (currentItem >=  $("fieldset li").length ) currentItem = 0; 
+		if (currentItem >=  $("fieldset li").length ) currentItem = 0; 		
+		
+		var item = $("#" + $("fieldset li")[currentItem].id );
+		
+		/* FUNCTIONAL KEYS */
+		if (event.which == 46 ) { // Delete
+		
+		}
+		
+		if (event.which == 13) {  // Enter
+			window.location.replace(item.prop("href"));
+		}		
+		
+
 		
 		$("fieldset li").removeClass("highlight");
 		$("fieldset li")[currentItem].className+=" highlight";
