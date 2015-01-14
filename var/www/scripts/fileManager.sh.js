a9508fd186290cbcc6edbac9125500951652aace
opalenzuela <opalenzuela@gmail.com>
2015-01-14 14:17:17 +0100

    Delaying initialization

--- var/www/scripts/fileManager.sh.js
+++ var/www/scripts/fileManager.sh.js
@@ -1,15 +1,17 @@
 var currentItem = -1;
-$("body").on("keydown",function(event){
-	console.log("Key pressed: "+ event.which);
-	if (event.which == 39 ) { // Right
-		currentItem++;
-	}
-	if (event.which == 37 ) { // Left
-		currentItem--;
-	}	
-	if (currentItem == -1) currentItem = $("li.item").length -1;
-	if (currentItem >=  $("li.item").length ) currentItem = 0; 
-	
-	$("fieldset li").removeClass("highlight");
-	$("fieldset li")[2].className+=" highlight";
+$(function($){
+	$("body").on("keydown",function(event){
+		console.log("Key pressed: "+ event.which);
+		if (event.which == 39 ) { // Right
+			currentItem++;
+		}
+		if (event.which == 37 ) { // Left
+			currentItem--;
+		}	
+		if (currentItem == -1) currentItem = $("li.item").length -1;
+		if (currentItem >=  $("li.item").length ) currentItem = 0; 
+		
+		$("fieldset li").removeClass("highlight");
+		$("fieldset li")[2].className+=" highlight";
+	});
 });
