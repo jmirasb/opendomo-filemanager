b7bf58a24d7f19da30192090003232de0fd3c112
opalenzuela <opalenzuela@gmail.com>
2015-02-09 15:41:19 +0100

    Splitting algorithm drives / directories

--- usr/local/opendomo/services/tools/fileManager.sh
+++ usr/local/opendomo/services/tools/fileManager.sh
@@ -18,82 +18,97 @@ if ! test -d "/media/$ROUTE"; then
     ROUTE="/"
 fi
 
-# Entering in route and see contents
-cd "/media/$ROUTE"
-if [ "$ROUTE" == "/" ]; then
+list_drives() {
     echo "#> Select drive"
 	echo "list:`basename $0`	detailed selectable"
 	DIRTYPE="drive"
-else
-    echo "#> Contents of [$ROUTE]"
-	echo "list:`basename $0`	iconlist"	
-	DIRTYPE="dir"
-fi
-
-for i in *; do
-    if test "$i" != "*"; then
-        e=`echo "$i" | cut -f2 -d.`
-        dn=`echo "$i" | sed 's/^.\///'`
-        bn=`basename "$i"`
-
-        if test -d "$i" 
-		then
-            # See folders
-            if test "$ROUTE" != "/"; then
-                echo "	-$ROUTE/$dn	$i	$DIRTYPE"
-            else
-				if test "$dn" != "lost+found"
-				then
-					echo "	-/$dn	$i	$DIRTYPE"
-				fi
-            fi
-        else
-			# See files
-			if test "$e" = "$EXT" || test -z "$EXT" ; then
-				case "$e" in
-                    jpg|jpeg|png|gif)
-                        type="file image"
-                    ;;
-                    avi|mpeg|mpg|mov|mp4|mjpg|webm|ogg)
-                        type="video"
-                    ;;
-                    txt|me|ME)
-                        type="file text"
-                    ;;
-                    htm|html)
-                        type="file html"
-                    ;;
-                    *)
-                        type="file"
-                    ;;
-                esac
-                if ! test -z "$PREFIX"; then
-                    dn=`echo "$dn" | grep "^$PREFIX"`
-                fi
-                if ! test -z "$SUFFIX"; then
-                    dn=`echo "$dn" | grep "$SUFFIX"`
-                fi
-                if ! test -z "$dn"; then
-                    echo "	-$ROUTE/$dn	$i	$type	"
-                fi
-            fi
-        fi
-    else
-        echo "#WARN This location is empty"
-    fi
-done
-
-echo "actions:"
-echo "	searchFiles.sh	Search"
-if test "$ROUTE" == "/"; then
+	cd /media/
+	for drive in * ; do
+		if test -f /media/$drive/opendomo.cfg; then
+			source /media/$drive/opendomo.cfg
+		fi
+		test -z "$LABEL" && LABEL=$drive
+		echo "	-/$drive	$LABEL	$drive	"
+	done
+	
+	echo "actions:"
+	echo "	searchFiles.sh	Search"
 	if test -x /usr/local/opendomo/umountDrive.sh; then
 		echo "	umountDrive.sh	Disconnect drive"
 	fi
-else
+}
+
+list_path_contents() {
+	ROUTE=$1
+    echo "#> Contents of [$ROUTE]"
+	echo "list:`basename $0`	iconlist"	
+	DIRTYPE="dir"
+	cd /media/$ROUTE
+	for i in *; do
+		if test "$i" != "*"; then
+			e=`echo "$i" | cut -f2 -d.`
+			dn=`echo "$i" | sed 's/^.\///'`
+			bn=`basename "$i"`
+
+			if test -d "$i" 
+			then
+				# See folders
+				if test "$ROUTE" != "/"; then
+					echo "	-$ROUTE/$dn	$i	$DIRTYPE"
+				else
+					if test "$dn" != "lost+found"
+					then
+						echo "	-/$dn	$i	$DIRTYPE"
+					fi
+				fi
+			else
+				# See files
+				if test "$e" = "$EXT" || test -z "$EXT" ; then
+					case "$e" in
+						jpg|jpeg|png|gif)
+							type="file image"
+						;;
+						avi|mpeg|mpg|mov|mp4|mjpg|webm|ogg)
+							type="video"
+						;;
+						txt|me|ME)
+							type="file text"
+						;;
+						htm|html)
+							type="file html"
+						;;
+						*)
+							type="file"
+						;;
+					esac
+					if ! test -z "$PREFIX"; then
+						dn=`echo "$dn" | grep "^$PREFIX"`
+					fi
+					if ! test -z "$SUFFIX"; then
+						dn=`echo "$dn" | grep "$SUFFIX"`
+					fi
+					if ! test -z "$dn"; then
+						echo "	-$ROUTE/$dn	$i	$type	"
+					fi
+				fi
+			fi
+		else
+			echo "#WARN This location is empty"
+		fi
+	done	
+	
+	echo "actions:"
 	echo "	goback	Back"
 	if test -x /usr/local/opendomo/deleteFiles.sh; then
 		echo "	deleteFiles.sh	Delete"
 	fi
-	echo "	searchFiles.sh	Search"
+	echo "	searchFiles.sh	Search"		
+}
+
+if [ "$ROUTE" == "/" ]; then
+	list_drives
+else
+	list_path_contents $ROUTE
 fi
+
 echo
