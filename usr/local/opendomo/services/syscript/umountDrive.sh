f905f7912ed52ae19ecf562f2f0406435c9d4ef4
opalenzuela <opalenzuela@gmail.com>
2015-02-10 17:12:41 +0100

    Modifying flow

--- usr/local/opendomo/services/syscript/umountDrive.sh
+++ usr/local/opendomo/services/syscript/umountDrive.sh
@@ -3,7 +3,7 @@
 #package:odfilemanager
 #type:local
 
-# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+# Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
 
 DRIVE="/media/$1"
 CONFFILE="$DRIVE/opendomo.cfg"
@@ -14,16 +14,16 @@ HOMEDEVICE=`grep ^HOMEDEVICE= $CONFFILE | sed 's/\"//g' | cut -f2 -d=`
 # Check system device
 if [ "$CONFDEVICE" = "1" ] || [ "$SYSDEVICE" = "1" ] || [ "$HOMEDEVICE" = "1" ]; then
     echo "#ERRO $DRIVE is a system device, can't be unmounted"
-    /usr/local/opendomo/fileManager.sh
 else
     # Unmount
     if
     sudo umount "$DRIVE"
     then
         echo "#INFO You can eject device"
-        /usr/local/opendomo/fileManager.sh
     else
         echo "#ERRO Device is busy, wait a moment and try again"
-        /usr/local/opendomo/fileManager.sh
     fi
 fi
+echo "actions:"
+echo "	goback	Back"
+echo
