7394cc966e5d274ac19186e226c641e29b162550
opalenzuela <opalenzuela@gmail.com>
2015-05-29 11:21:48 +0200

    First trial inserting in DB

--- usr/local/opendomo/bin/driveindex2db.sh
+++ usr/local/opendomo/bin/driveindex2db.sh
@@ -25,10 +25,28 @@ then
 	echo "#INFO Drive is being scanned"
 	exit 0
 fi
-if test -f .scanfile.txt
+if ! test -f .scanfile.txt
 then
-	cat .scanfile.txt
-else
 	echo "#ERROR Drive was never indexed"
 	exit 1
 fi
+
+if ! test -f .scanfile.sqlite
+then
+	CREATESQL="CREATE TABLE files (id INTEGER PRIMARY KEY AUTOINCREMENT, md5 CHAR(32), size INTEGER, path TEXT);"
+	echo $CREATESQL | sqlite3 .scanfile.sqlite
+fi
+
+TOTAL=`wc -l .scanfile.txt | cut -f1 -d' '`
+CURRENT=0
+while read LINE
+do
+	let CURRENT=$CURRENT+1
+	#TODO use AWK instead
+	MD5SUM=`echo $LINE | cut -f1 -d' '`
+	SIZE=`echo $LINE | cut -f2 -d' '`
+	PATH=`echo $LINE | cut -f3- -d' '`
+	echo "REPLACE INTO files (md5,size,path) VALUES ('$MD5SUM',$SIZE, '$FILENAME')" | sqlite3 .scanfile.sqlite
+	echo "# Updating $FILENAME ... ($CURRENT / $TOTAL)"
+	
+done < .scanfile.txt
