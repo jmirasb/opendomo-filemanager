61628b4924869d2af9b05ab9ad3ce89bf5ec0687
opalenzuela <opalenzuela@gmail.com>
2015-06-02 15:33:31 +0200

    Uploadfile script

---
+++ usr/local/opendomo/services/syscript/uploadFile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#desc:Upload file
+#package:odfilemanager
+#type:local
+
+# Copyright(c) 2015 OpenDomo Services SL. Licensed under GPL v3 or later
+
+FILE="/media/$1"
+
+
+echo "actions:"
+echo "	goback	Back"
+echo
